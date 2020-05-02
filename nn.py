c1e8a4eeef9d22b2f259435630f702bb3f94eb5f
sevendvm <sd@sevendvm.ru>
2020-05-03 00:33:09 +0400

    initial commit

---
+++ nn.py
@@ -0,0 +1,95 @@
+import random
+from math import exp
+
+
+class NeuralNetwork:
+	def __init__(self, inputs_count, hidden_count, outputs_count, activation_function, learning_rate):
+		self.inputs_count = inputs_count
+		self.hidden_count = hidden_count
+		self.outputs_count = outputs_count
+		
+		self.activation_function = activation_function
+		
+		self.learning_rate = learning_rate
+		
+		self.weights_input_hidden = [[random.random() * 2 - 1 for j in range(self.inputs_count)] for i in
+									 range(self.hidden_count)]
+		
+		self.weights_hidden_outputs = [[random.random() * 2 - 1 for j in range(self.hidden_count)] for i in
+									   range(self.outputs_count)]
+		
+		self.input_bias = [random.random() for i in range(self.hidden_count)]
+		self.hidden_bias = [random.random() for i in range(self.outputs_count)]
+		
+		self.input = [0 for i in range(self.inputs_count)]
+		self.hidden = [0 for i in range(self.hidden_count)]
+		self.output = [0 for i in range(self.outputs_count)]
+		
+	def feed_forward(self):
+		# evaluate hidden layer
+		for j in range(self.hidden_count):
+			summ = 0
+			for i in range(self.inputs_count):
+				summ += self.input[i] * self.weights_input_hidden[j][i]
+			self.hidden[j] = self.activation_function(summ + self.input_bias[j])
+			
+		# evaluate output
+		for j in range(self.outputs_count):
+			summ = 0
+			for i in range(self.hidden_count):
+				summ += self.hidden[i] * self.weights_hidden_outputs[j][i]
+			self.output[j] = self.activation_function(summ + self.hidden_bias[j])
+	
+	def guess(self, inputs):
+		self.input = [float(inputs[i]) for i in range(self.inputs_count)]
+		self.feed_forward()
+		return self.output
+	
+	def train(self, inputs, outputs):
+		# given an input
+		# guess an answer
+		guess = self.guess(inputs)
+		
+		# evaluate an error
+		error = [float(outputs[i]) - guess[i] for i in range(self.outputs_count)]
+		
+		return error
+		# back propagate an error to correct weights
+
+
+def sigmoid(x):
+	return 1 / (1 + exp(-x))
+
+
+def main():
+	nn = NeuralNetwork(7, 12, 10, activation_function=sigmoid, learning_rate=1)
+	# nn = NeuralNetwork(2, 3, 2)
+	
+	labeled_data = {0: [[1, 1, 1, 1, 1, 1, 0],  # stands for 0 in 7-segment display
+						[1, 0, 0, 0, 0, 0, 0, 0, 0, 0]],  # zero output should be active
+					1: [[0, 0, 1, 1, 0, 0, 0],
+						[0, 1, 0, 0, 0, 0, 0, 0, 0, 0]],
+					2: [[0, 1, 1, 0, 1, 1, 1],
+						[0, 0, 1, 0, 0, 0, 0, 0, 0, 0]],
+					3: [[0, 1, 1, 1, 1, 0, 1],
+						[0, 0, 0, 1, 0, 0, 0, 0, 0, 0]],
+					4: [[1, 0, 1, 1, 0, 0, 1],
+						[0, 0, 0, 0, 1, 0, 0, 0, 0, 0]],
+					5: [[1, 1, 0, 1, 1, 0, 1],
+						[0, 0, 0, 0, 0, 1, 0, 0, 0, 0]],
+					6: [[1, 1, 0, 1, 1, 1, 1],
+						[0, 0, 0, 0, 0, 0, 1, 0, 0, 0]],
+					7: [[1, 1, 1, 1, 0, 0, 0],
+						[0, 0, 0, 0, 0, 0, 0, 1, 0, 0]],
+					8: [[1, 1, 1, 1, 1, 1, 1],
+						[0, 0, 0, 0, 0, 0, 0, 0, 1, 0]],
+					9: [[1, 1, 1, 1, 1, 0, 1],
+						[0, 0, 0, 0, 0, 0, 0, 0, 0, 1]]}
+	
+	print(nn.train(labeled_data[0][0], labeled_data[0][1]))
+	print(nn.input)
+	print(nn.output)
+
+
+if __name__ == '__main__':
+	main()
