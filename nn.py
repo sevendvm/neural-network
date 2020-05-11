19282e5ae6b91752e456a17d100668b84c56be56
sevendvm <sd@sevendvm.ru>
2020-05-11 11:48:29 +0400

    добавлена процедура обратного распространения ошибки
    added backpropagation

--- nn.py
+++ nn.py
@@ -1,4 +1,5 @@
 import random
+from functools import reduce
 from math import exp
 
 
@@ -24,7 +25,7 @@ class NeuralNetwork:
 		self.input = [0 for i in range(self.inputs_count)]
 		self.hidden = [0 for i in range(self.hidden_count)]
 		self.output = [0 for i in range(self.outputs_count)]
-		
+	
 	def feed_forward(self):
 		# evaluate hidden layer
 		for j in range(self.hidden_count):
@@ -32,7 +33,7 @@ class NeuralNetwork:
 			for i in range(self.inputs_count):
 				summ += self.input[i] * self.weights_input_hidden[j][i]
 			self.hidden[j] = self.activation_function(summ + self.input_bias[j])
-			
+		
 		# evaluate output
 		for j in range(self.outputs_count):
 			summ = 0
@@ -45,20 +46,65 @@ class NeuralNetwork:
 		self.feed_forward()
 		return self.output
 	
-	def train(self, inputs, outputs):
+	def train(self, inputs, targets):
 		# given an input
 		# guess an answer
 		guess = self.guess(inputs)
 		
 		# evaluate an error
-		error = [float(outputs[i]) - guess[i] for i in range(self.outputs_count)]
+		error_vector = [(float(targets[i]) - guess[i]) ** 2 / 2 for i in range(self.outputs_count)]
 		
-		return error
+		# calculate a total error, which eventually
+		# could have been a fitness measurement for
+		# genetic learning algorithm
+		total_error = reduce(lambda a, b: a + b, error_vector)
+		
 		# back propagate an error to correct weights
+		if total_error > 0:
+			# for each weight calculate the following expression:
+			# dE/dw = dE/dout * dout/dnet * dnet/dw
+			#  [[0 for j in range(self.hidden_count)] for i in range(self.outputs_count)]
+			correction = [[0] * self.hidden_count] * self.outputs_count
+			for j in range(self.outputs_count):
+				for i in range(self.hidden_count):
+					correction[j][i] = -(targets[j] - self.output[j]) * \
+										self.output[j] * (1 - self.output[j]) * \
+										self.hidden[i]
+			
+			# print("hidden to outputs correction")
+			# print(correction)
+			
+			# perform an actual correction
+			for i in range(self.outputs_count):
+				for j in range(self.hidden_count):
+					self.weights_hidden_outputs[i][j] = self.weights_hidden_outputs[i][j] - \
+														self.learning_rate * correction[i][j]
+			
+			# calculate the correction matrix for inputs to hidden layer
+			# correction = [[0 for i in range(self.inputs_count)] for j in range(self.hidden_count)]
+			correction = [[0] * self.inputs_count] * self.hidden_count
+			for j in range(self.hidden_count):
+				for i in range(self.inputs_count):
+					# TODO: implement a correction calculation
+					correction[j][i] = 0  # -(targets[j] - self.output[j]) * \
+										# self.output[j] * (1 - self.output[j]) * \
+										# self.hidden[i]
+			
+			# perform an actual correction
+			for i in range(self.hidden_count):
+				for j in range(self.inputs_count):
+					self.weights_input_hidden[i][j] = self.weights_input_hidden[i][j] - \
+														self.learning_rate * correction[i][j]
+		
+		return error_vector, total_error
 
 
 def sigmoid(x):
 	return 1 / (1 + exp(-x))
+
+
+def sigmoid_derivative(x):
+	return x * (1 - x)
 
 
 def main():
